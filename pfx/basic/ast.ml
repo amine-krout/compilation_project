8feae3084186157d48464502894603c0fb457148
amine-krout <mohamedamine.krout@supcom.tn>
2023-03-14 13:56:47 +0100

    first commit

---
+++ pfx/basic/ast.ml
@@ -0,0 +1,27 @@
+type command =
+  (* DefineMe (* Question 4.1 *) *)
+  | PUSH of int
+  | ADD
+  | SUB
+  | MULT
+  | DIV
+  | POP (* Remove element *)
+  | SWAP (* Swap elements *)
+	
+type program = int * command list
+
+(* add here all useful functions and types  related to the AST: for instance  string_of_ functions *)
+
+let string_of_command = function
+  | PUSH n -> "PUSH" ^ string_of_int n 
+  | ADD -> "ADD"
+  | SUB -> "SUB" 
+  | MULT -> "MULT"
+  | DIV -> "DIV"
+  | POP -> "POP"
+  | SWAP -> "SWAP"
+
+let string_of_commands cmds = String.concat " " (List.map string_of_command cmds)
+
+let string_of_program (args, cmds) = Printf.sprintf "%i args: %s\n" args (string_of_commands cmds)
+
---
+++ pfx/basic/eval.ml
@@ -0,0 +1,74 @@
+open Ast
+open Printf
+
+let string_of_stack stack = sprintf "[%s]" (String.concat ";" (List.map string_of_int stack))
+
+let string_of_state (cmds,stack) =
+  (match cmds with
+   | [] -> "no command"
+   | cmd::_ -> sprintf "executing %s" (string_of_command cmd))^
+    (sprintf " with stack %s" (string_of_stack stack))
+
+(* Question 4.2 *)
+let step state =
+  match state with
+  | [], _ -> Error("Nothing to step",state)
+  (* Valid configurations *)
+  
+  | PUSH n :: q , stack -> Ok (q, n::stack)
+  
+  | ADD :: q, stack ->
+  ( match stack with 
+    | x :: y :: s -> Ok (q, (x+y) :: s)
+    | _ -> Error("Not enough arguments for add")
+  )
+  
+  | SUB :: q, stack ->
+  ( match stack with 
+    | x :: y :: s -> Ok (q, (x-y) :: s)
+    | _ -> Error("Not enough arguments for sub")
+  ) 
+  
+  | MULT :: q, stack -> 
+  ( match stack with 
+    | x :: y :: s -> Ok (q, (x*y) :: s)
+    | _ -> Error("Not enough arguments for MULT")
+  )
+  
+  | DIV :: q, stack ->
+  ( match stack with 
+    | x :: y :: s -> Ok (q, (x/y) :: s)
+    | _ -> Error("Not enough arguments for DIV")
+  )  
+  
+  | POP :: q, stack -> 
+  ( match stack with 
+    | x :: s -> Ok (q, s)
+    | _ -> Error("Empty stack")
+  ) 
+  
+  | SWAP :: q, stack -> 
+  ( match stack with 
+    | x :: y :: s -> Ok (q, y :: x :: s)
+    | _ -> Error("Not enough arguments for SWAP")
+  )
+  
+  (* | DefineMe :: q , stack          -> Ok (q, stack) *)
+
+let eval_program (numargs, cmds) args =
+  let rec execute = function
+    | [], []    -> Ok None
+    | [], v::_  -> Ok (Some v)
+    | state ->
+       begin
+         match step state with
+         | Ok s    -> execute s
+         | Error e -> Error e
+       end
+  in
+  if numargs = List.length args then
+    match execute (cmds,args) with
+    | Ok None -> printf "No result\n"
+    | Ok(Some result) -> printf "= %i\n" result
+    | Error(msg,s) -> printf "Raised error %s in state %s\n" msg (string_of_state s)
+  else printf "Raised error \nMismatch between expected and actual number of args\n"
